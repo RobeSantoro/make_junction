0910f9ffaf4c65c9e6974fe26e3a9c5e8a72a028
RobeSantoro <santoro.robe@gmail.com>
2023-11-20 13:56:24 +0100

    Add explainer

--- make_junction.py
+++ make_junction.py
@@ -1,4 +1,50 @@
+
+"""
+This script creates a directory junction on Windows, essentially a symbolic link, for a Blender addon. 
+The junction links the addon from its source location to the desired Blender version's addons folder. 
+This automates the process of setting up Blender addons, especially when addons are being developed 
+and frequently updated in a separate working directory.
+
+Usage:
+------
+The script takes three main arguments:
+
+1. Blender Version: Specify the Blender version's addons directory where the junction should be created.
+   Use the `-b` or `--blender_version` flag followed by the Blender version string (e.g., "3.4").
+   
+2. Addon Name: Specify the exact name of the addon folder you want to create the junction for.
+   Use the `-a` or `--addon_name` flag followed by the addon name string.
+
+3. Source Location (Optional): Specify the directory where the addon is located.
+   Use the `-s` or `--source` flag followed by the source directory.
+   If this argument is omitted, it defaults to "L:\\BLENDER\\ADDONS\\".
+
+The script requires the `blender_version` and `addon_name` to be specified in order to create the junction.
+If no arguments are specified, the script will print out the help message and terminate.
+
+Examples:
+---------
+Creating a junction for an addon called "blender-2.80-3ds-io-3ds" for Blender version 3.4:
+
+    python junction_for_blender_addon.py -b 3.4 -a blender-2.80-3ds-io-3ds
+
+Creating a junction with a specified source location:
+
+    python junction_for_blender_addon.py -b 3.4 -a blender-2.80-3ds-io-3ds -s C:\\MyAddons\\
+
+Note:
+-----
+This script is designed to work on Windows due to the use of the `mklink` command.
+The Blender addons folder is typically located in the user's AppData folder (accessed with %APPDATA%),
+and this script constructs that path dynamically for the specified Blender version.
+
+The script also outputs the constructed paths and the actual command line string that it will use to create the junction.
+Please ensure you have the necessary permissions to create junctions on the system.
+"""
+
+
 import os
+import sys
 import argparse
 
 parser = argparse.ArgumentParser(description='Create a junction for a blender addon in the specified blender addons folder')
@@ -19,6 +65,11 @@ parser.add_argument('-s', '--source', metavar='SOURCE', type=str, nargs='+', def
                     help='the source location of the addon, the addon folder. Default is "L:\\BLENDER\\ADDONS\\"')
 
 args = parser.parse_args()
+
+# If no arguments are passed, print the help
+if len(sys.argv) == 1:
+    parser.print_help(sys.stderr)
+    sys.exit(1)
 
 blender_version = args.blender_version[0]
 print('blender_version: ' + blender_version)
